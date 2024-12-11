42c32d7ad8a8cdfab021962ba1e6be1837e01376
nextuser <nextuser@163.com>
2024-12-11 09:39:08 +0800

    remove key in source , add local_key to read ~/.sui/sui_config/sui.keystore

--- webapp/client/client-devnet.ts
+++ webapp/client/client-devnet.ts
@@ -7,15 +7,16 @@ import { Ed25519Keypair } from '@mysten/sui/keypairs/ed25519';
 import { Transaction } from '@mysten/sui/transactions';
 import { fromBase64,toBase64 } from '@mysten/bcs';
 import * as fs from 'fs';
+import {get_key } from './local_key';
 
 dotenv.config();
 // 初始化SUI Client, 用于和主网(mainnet)交互
 const suiClient = new SuiClient({ url: getFullnodeUrl('devnet') });
 // 从环境变量读取secretKey
-const secretKey = process.env.SECRET_KEY || 'AFof4r1AYwrMtfOoJ8lwU4ewYec9YHC5UQGwXn78nnuE';
+const secretKey = process.env.SECRET_KEY || get_key();;
 /** 这里把base64编码的secretKey转换为字节数组后截掉第一个元素，是因为第一位是一个私钥类型的标记位，后续派生签名者时不需要 **/
 const secretKeyBytes = fromBase64(secretKey).slice(1); // 发起方账户私钥
-const signer = Ed25519Keypair.fromSecretKey(secretKeyBytes, ); // 生成签名者
+const signer = Ed25519Keypair.fromSecretKey(secretKeyBytes ); // 生成签名者
 
 type UserBonus ={
     id : string,
@@ -89,10 +90,10 @@ function show_period( period :BonusPeriodWrapper){
 }
 /**
 async function ctf_check_in(flag_str : string, github_id: string) {
-    // const secretKey = process.env.SECRET_KEY || 'AFof4r1AYwrMtfOoJ8lwU4ewYec9YHC5UQGwXn78nnuE';
+    // const secretKey = process.env.SECRET_KEY || get_key();
     //  这里把base64编码的secretKey转换为字节数组后截掉第一个元素，是因为第一位是一个私钥类型的标记位，后续派生签名者时不需要 
     // const secretKeyBytes = fromBase64(secretKey).slice(1); // 发起方账户私钥
-    // const signer = Ed25519Keypair.fromSecretKey(secretKeyBytes, ); // 生成签名者
+    // const signer = Ed25519Keypair.fromSecretKey(secretKeyBytes ); // 生成签名者
     const tx = new Transaction();
     tx.moveCall({
         target: `${consts.ctf_package_id}::ctf_checkin_try::my_flag`,
