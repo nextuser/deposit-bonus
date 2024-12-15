3121e95699f5a7c6568502b693299f81ec443cbb
nextuser <nextuser@163.com>
2024-12-15 21:50:07 +0800

    remove old

--- old/contracts/deposit_bonus/pub-dev.sh
+++
@@ -1,2 +0,0 @@
-sui client switch --address admin
-sui client publish |tee ./log/publish-devnet.log
--- old/webapp/navi-demo/src/services/type.ts
+++
@@ -1,5 +0,0 @@
-export interface DataType {
-  address: string;
-  reward: string;
-  multiplier: string;
-}
